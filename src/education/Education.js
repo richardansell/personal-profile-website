d09f44d8d007fead914df54dd6e1024fa21a119a
Richard Ansell <ransell83@gmail.com>
2019-03-13 14:16:42 +0000

    Minor non-branch related edit (feature/education), involving setting the grid breakpoints

--- src/education/Education.js
+++ src/education/Education.js
@@ -66,7 +66,7 @@ class Education extends Component {
         return (
             <div ref={this.educationRef}>
                 <Grid alignItems="flex-start" container justify="center" spacing={24}>
-                    <Grid item md={12}>
+                    <Grid item md={12} xs={12}>
                         <Typography color="secondary" gutterBottom
                                     variant={widthSmDown ? "h5" : "h4"}>
                             Education
